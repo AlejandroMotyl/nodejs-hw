d92a0fc6fbad097ac6fb3f2fd20a59c3828b4dd9
Olexander Motyl <JustSanya1@gmail.com>
2025-11-10 22:29:22 +0100

    sendEmail fix

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -1,14 +1,14 @@
 import nodemailer from 'nodemailer';
-
-const transporter = nodemailer.createTransport({
-  host: process.env.SMTP_HOST,
-  port: process.env.SMTP_PORT,
-  auth: {
-    user: process.env.SMTP_USER,
-    pass: process.env.SMTP_PASSWORD,
-  },
-});
-
 export const sendEmail = async (options) => {
+  const transporter = nodemailer.createTransport({
+    host: process.env.SMTP_HOST,
+    port: parseInt(process.env.SMTP_PORT, 10),
+    secure: false,
+    auth: {
+      user: process.env.SMTP_USER,
+      pass: process.env.SMTP_PASSWORD,
+    },
+  });
+
   return await transporter.sendMail(options);
 };
